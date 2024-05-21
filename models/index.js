dd587b90afed2738db58612a561f0d4c9df326b5
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-05-21 16:34:12 -0500

    finished models

--- models/index.js
+++ models/index.js
@@ -6,23 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: '',
-})
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: '',
-})
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-
-})
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: '',
-})
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
