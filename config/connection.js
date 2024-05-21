0566210c4b4a86243c1f6ea3a31130e8827c28dc
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-05-21 16:17:23 -0500

    env

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  : new Sequelize(process.env.DB_NAME, 'postgres', process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'postgres',
       dialectOptions: {
